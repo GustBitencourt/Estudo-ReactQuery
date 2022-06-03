05eb5dd485442f61793b98f493b4e50aef724f97
GustBitencourt <gubiten@gmail.com>
2022-06-03 15:45:36 -0300

    funcao refetch

--- src/pages/RQSuperHeroes/index.jsx
+++ src/pages/RQSuperHeroes/index.jsx
@@ -7,20 +7,26 @@ const fetchSuperHeroes = () => {
 
 export const RQSuperHeroes = () => {
   //desestrutra os dados do useQuery
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "heroes",
     fetchSuperHeroes,
     {
-      //cacheTime: 5000, //tempo de duração da query de requisição permanecer guardada em cache após, sair da página e ir pro lixo - tempo default é de 5 minutos
-      staleTime: 30000, //tempo em que o data será considera "fresco" - o valor default é 0
-      //refetchOnMount: true, //refetch data on mount of component - similar a forma tradicional
-      refetchOnWindowFocus: true, //refetch data on window focus - atualiza o dado quando saimos e voltamos da tela - dafault é true
+      // -> cacheTime: 5000, //tempo de duração da query de requisição permanecer guardada em cache após, sair da página e ir pro lixo - tempo default é de 5 minutos
+      // -> staleTime: 30000, //tempo em que o data será considera "fresco" - o valor default é 0
+      // -> refetchOnMount: true, //refetch data on mount of component - similar a forma tradicional
+      // -> refetchOnWindowFocus: true, //refetch data on window focus - atualiza o dado quando saimos e voltamos da tela - dafault é true
+
+      // POOLING
+      // -> refetchInterval: 2000, //refetch data on interval - atualiza o dado a cada 2 segundos seu valor default é false, é desativado quando o usuário sai da janela
+      // -> refetchIntervalInBackground: 2000, //refetch data on interval - atualiza o dado a cada 2 segundos seu valor default é false, funciona mesmo quando o usuário sai da janela
+
+      enabled: false, //desabilita o carregamneto do cache - pode reabilitar através da função refetch do useQuery
     }
   );
 
 
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
 
@@ -31,6 +37,7 @@ export const RQSuperHeroes = () => {
   return (
     <div>
       <h1>RQSuperHeroes</h1>
+      <button onClick={refetch}>Fetch</button>
 
       {
         //confirmar a existencia do data
