440405894c6077865206c6bead0f9ce0db4bca23
GustBitencourt <gubiten@gmail.com>
2022-06-03 11:06:37 -0300

    error handle tradicional e com useQuery

--- src/pages/RQSuperHeroes/index.jsx
+++ src/pages/RQSuperHeroes/index.jsx
@@ -1,34 +1,35 @@
-import { useQuery } from 'react-query'
-import axios from 'axios'
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const fetchSuperHeroes = () => {
-  return axios.get('http://localhost:4000/superheroes')
-
-}
+  return axios.get("http://localhost:4000/superheroe");
+};
 
 export const RQSuperHeroes = () => {
   //desestrutra os dados do useQuery
-  const { isLoading, data } = useQuery('heroes', fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    "heroes",
+    fetchSuperHeroes
+  );
 
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
 
   return (
     <div>
       <h1>RQSuperHeroes</h1>
 
       {
-        isLoading ? (
-          <div>Loading...</div>
-
-        ) : (
-          //confirmar a existencia do data
-          data?.data.map((hero) => (
-            <div key={hero.id} >
-              {hero.name}
-            </div>
-          ))
-        )
+        //confirmar a existencia do data
+        data?.data.map((hero) => (
+          <div key={hero.id}>{hero.name}</div>
+        ))
       }
     </div>
   );
-}
+};
--- src/pages/SuperHeroes/index.jsx
+++ src/pages/SuperHeroes/index.jsx
@@ -1,36 +1,41 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const SuperHeroes = () => {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const [heroesData, setHeroesData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes')
-      .then(res => {
-        setHeroesData(res.data)
-        setIsLoading(false)
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setHeroesData(res.data);
+        setIsLoading(false);
       })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, []);
 
-  }, [])
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
-  
+  if (error) {
+    return <h2>{error}</h2>;
+  }
+
   return (
     <div>
       <h1>SuperHeroes</h1>
 
       {
-        isLoading ? (
-          <div>Loading...</div>
-
-        ) : (
-          heroesData.map((hero) => (
-            <div key={hero.id} >
-              {hero.name}
-            </div>
-          ))
-        )
+        heroesData.map((hero) => (
+          <div key={hero.id}>{hero.name}</div>
+        ))
       }
     </div>
   );
-}
+};
