848c2f31014312b982b5bc787234c60e87f65484
GustBitencourt <gubiten@gmail.com>
2022-06-06 09:55:29 -0300

    function select

--- src/pages/RQSuperHeroes/index.jsx
+++ src/pages/RQSuperHeroes/index.jsx
@@ -8,11 +8,10 @@ const fetchSuperHeroes = () => {
 export const RQSuperHeroes = () => {
   const onSucess = (data) => {
     console.log("Efeito colateral after data fetching", data);
-  
-  }
+  };
   const onError = (error) => {
     console.log("Efeito colateral after encountering some error", error);
-  }
+  };
 
   //desestrutra os dados do useQuery
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
@@ -34,11 +33,12 @@ export const RQSuperHeroes = () => {
       onSucess: onSucess,
       onError: onError,
 
-
+      select: (data) => {
+        const selectHeroNames = data.data.map((hero) => hero.name);
+        return selectHeroNames;
+      }, // function select transforma os dados recebidos da api alterando sua estrura - pode se usar filter também
     }
   );
-
-
 
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
@@ -50,13 +50,22 @@ export const RQSuperHeroes = () => {
 
   return (
     <div>
-      <h1>RQSuperHeroes</h1>
+      {/* <h1>RQSuperHeroes</h1>
       <button onClick={refetch}>Fetch</button>
 
       {
         //confirmar a existencia do data
         data?.data.map((hero) => (
           <div key={hero.id}>{hero.name}</div>
+        ))
+      } */}
+
+      <h1>RQSuperHeroes - function select</h1>
+
+      {
+        //só é necessário um data a partir do select
+        data.map((heroName) => (
+          <div key={heroName}>{heroName}</div>
         ))
       }
     </div>
